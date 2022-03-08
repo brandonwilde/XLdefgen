97be8cb1655213572f0dfbc143cd4a936df9db3b
Brandon Wilde <74289641+brandonwilde@users.noreply.github.com>
2022-03-08 18:07:33 -0500

    Add ability to mark definiendum in sample sentence

--- data/clean_test_data.py
+++ data/clean_test_data.py
@@ -14,7 +14,7 @@ data = pd.read_csv(file)
 # Keep only exact matches (case-insensitive)
 data['keep1'] = data.apply(lambda x: True if str(x['Wort']).lower() in x['Beispiel'].lower() else False, axis=1)
 
-# Keep fuzzy matches (<= 2 character differences)
+# Keep fuzzy matches (<= 1 character differences)
 data['keep2'] = data.apply(
     lambda x: True if re.search("(?b)(" + str(x['Wort']).lower()
                                 + "){e<=1}", str(x['Beispiel']).lower())
@@ -35,3 +35,40 @@ for diff in diffs[0][20:30]:
     print(data['Wort'].iloc[diff])
     print(data['Beispiel'].iloc[diff])
     print()
+    
+
+def matcher(series):
+    '''Mark definiendum for exact matches.
+    May still include affixes.'''
+
+    sent = series['Beispiel']
+    word = series['Wort']
+    
+    # Get start index of match
+    start = sent.lower().find(str(word).lower())
+    if start != -1:
+        try:    # Get end index of match (may need to account for affixes)
+            end = start + re.search('\W', sent[start:]).span()[0]
+        except: # Definiendum is at end of string
+            return sent[:start]+'*'+sent[start:]+'*'
+        else:
+            return sent[:start]+'*'+sent[start:end]+'*'+sent[end:]
+    else: # No exact match
+        return False
+
+
+# def matcher_fuzzy(series):
+    # """Mark definiendum for fuzzy matches.
+    # Allows up to one 'error' per match."""
+    
+#     match = re.search("(?b)(" + str(series['Wort']).lower()
+#                                 + "){e<=1}", str(series['Beispiel']).lower())
+#     if match:
+#         sent = series['Beispiel']
+#         s = match.span()
+#         return sent[:s[0]]+'*'+sent[s[0]:s[1]]+'*'+sent[s[1]:]
+#     else:
+#         return False
+    
+data['keep3'] = data.apply(matcher, axis=1)
+print(data)
