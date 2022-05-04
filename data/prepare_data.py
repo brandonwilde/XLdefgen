c00c5e7fa575f37728e396e1174d7552c17854fc
Brandon Wilde <74289641+brandonwilde@users.noreply.github.com>
2022-05-03 22:34:10 -0400

    Cleaned prepare_data.py, updated --prepend
    --prepend now prepends actual word form used in example sentence rather than word given in 'word' column.

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -87,77 +87,18 @@ def parse_args():
 
     args = parser.parse_args()
     
-    # assert args.lang in ["en", "de"], "language must be 'en' or 'de'."
-
     return args
 
-# Input args to specify train/val/test split - which should be included
-# and how much data to put in each split
 
-def matcher_0(series, lang="en", demarcator="*"):
-    '''Mark definiendum for exact matches.
-    Will still include affixes if present.'''
-    
-    sent = str(series[lang+'_example'])
-    word = str(series[lang+'_word'])
-    
-    # print(word)
-    # print(sent)
-    
-    # Get start index of match
-    match = re.search(word.lower(), sent.lower())
-    if match:
-        
-        b_match = match.span()[0]+1
-        e_match = match.span()[1]-1
-        
-        try:    # Get start index of word (may need to account for prefix)
-            start = b_match - re.search('\W', sent[:b_match][::-1]).span()[0]
-        except:
-            start = 0
-        
-        try:    # Get end index of match (may need to account for suffixes)
-            end = e_match + re.search('\W', sent[e_match:]).span()[0]
-        except:
-            end = len(sent)
-            
-        return sent[:start]+' '+demarcator+' '+sent[start:end]+' '+demarcator+' '+sent[end:]
-    
-    else: # No match
-        return np.nan
-
-
-def matcher_1(series, lang="en", demarcator="*"):
+def matcher(series, lang="en", allow=0, mark=False, prepend=False, demarcator="*"):
     """Mark definiendum for fuzzy matches.
     Allows up to one 'error' per match."""
            
-    sent = str(series[lang+'_example'])
     word = str(series[lang+'_word'])
+    sent = str(series[lang+'_example'])
+    mod_sent = sent
     
-    if len(word) > 4:
-        match = re.search("(?b)(" + word.lower() + "){e<=1}", sent.lower())
-
-        if match:
-            
-            b_match = match.span()[0]+1
-            e_match = match.span()[1]-1
-            
-            try:    # Get start index of word (may need to account for prefix)
-                start = b_match - re.search('\W', sent[:b_match][::-1]).span()[0]
-            except:
-                start = 0
-            
-            try:    # Get end index of match (may need to account for suffixes)
-                end = e_match + re.search('\W', sent[e_match:]).span()[0]
-            except:
-                end = len(sent)
-                
-            return sent[:start]+' '+demarcator+' '+sent[start:end]+' '+demarcator+' '+sent[end:]
-        
-        else: # No match
-            return np.nan
-    
-    else: # short word needs exact match
+    if allow == 0 or len(word) < 5: # short word needs exact match
         match = re.search(word.lower(), sent.lower())
         if match:
             
@@ -173,22 +114,22 @@ def matcher_1(series, lang="en", demarcator="*"):
                 end = e_match + re.search('\W', sent[e_match:]).span()[0]
             except:
                 end = len(sent)
-                
-            return sent[:start]+' '+demarcator+' '+sent[start:end]+' '+demarcator+' '+sent[end:]
+            
+            word_match = sent[start:end]
+            
+            if mark:
+                mod_sent = sent[:start]+' '+demarcator+' '+word_match+' '+demarcator+' '+sent[end:]
+            
+            if prepend:
+                mod_sent = word_match + '. ' + mod_sent
+            
+            return mod_sent
         
         else: # No match
             return np.nan
         
-def matcher_2(series, lang="en", demarcator="*"): # This seems to let in too many false positive matches (about half of the 67 added)
-    """Mark definiendum for fuzzy matches.
-    Allows up to two 'errors' per match."""
-            
-    sent = str(series[lang+'_example'])
-    word = str(series[lang+'_word'])
-    
-    if len(word) > 4:
-        match = re.search("(?b)(" + word.lower() + "){e<=2}", sent.lower())
-
+    else: # longer words may have some variation
+        match = re.search("(?b)(" + word.lower() + "){e<=" + str(allow) + "}", sent.lower())
         if match:
             
             b_match = match.span()[0]+1
@@ -204,37 +145,21 @@ def matcher_2(series, lang="en", demarcator="*"): # This seems to let in too man
             except:
                 end = len(sent)
                 
-            return sent[:start]+' '+demarcator+' '+sent[start:end]+' '+demarcator+' '+sent[end:]
+            word_match = sent[start:end]
+            
+            if mark:
+                mod_sent = sent[:start]+' '+demarcator+' '+word_match+' '+demarcator+' '+sent[end:]
+            
+            if prepend:
+                mod_sent = word_match + '. ' + mod_sent
+            
+            return mod_sent
         
         else: # No match
             return np.nan
     
-    else: # short word needs exact match
-        match = re.search(word.lower(), sent.lower())
-        if match:
-            
-            b_match = match.span()[0]+1
-            e_match = match.span()[1]-1
-            
-            try:    # Get start index of word (may need to account for prefix)
-                start = b_match - re.search('\W', sent[:b_match][::-1]).span()[0]
-            except:
-                start = 0
-            
-            try:    # Get end index of match (may need to account for suffixes)
-                end = e_match + re.search('\W', sent[e_match:]).span()[0]
-            except:
-                end = len(sent)
-                
-            return sent[:start]+' '+demarcator+' '+sent[start:end]+' '+demarcator+' '+sent[end:]
-        
-        else: # No match
-            return np.nan
 
-
-def main():
-    # Parse the arguments
-    args = parse_args()
+def main(args):
     
     if args.input_file is None: # Or if not running from command line
         in_file = "C:/Users/brand/Documents/Projects/XLdefgen/data/codwoe_train_en.csv"
@@ -245,28 +170,18 @@ def main():
     start_length = len(data)
     
     print(data)
-    
-    # Set default column for concatenation
-    concat_col = args.source_lang + "_example"
-    
-    if args.mark:     
-        if args.allow == 0:
-            data[args.source_lang+'_marked'] = data.apply(matcher_0, lang=args.source_lang, demarcator=args.demarcator, axis=1)
-        elif args.allow == 1:
-            data[args.source_lang+'_marked'] = data.apply(matcher_1, lang=args.source_lang, demarcator=args.demarcator, axis=1)
-        elif args.allow == 2:
-            data[args.source_lang+'_marked'] = data.apply(matcher_2, lang=args.source_lang, demarcator=args.demarcator, axis=1)
-        
-        data['input'] = data[args.source_lang+'_marked']
-        concat_col = args.source_lang + "_marked"
-    
-    if args.prepend:
-        data[args.source_lang+'_prepend'] = data.apply(lambda x: str(x[args.source_lang+'_word']) + '. ' + str(x[concat_col]), axis=1)
-        data['input'] = data[args.source_lang+'_prepend']
+       
+    data['input'] = data.apply(matcher,
+                                lang=args.source_lang,
+                                allow=args.allow,
+                                mark=args.mark,
+                                prepend=args.prepend,
+                                demarcator=args.demarcator,
+                                axis=1)
     
     data['target'] = data[args.target_lang+'_gloss']
     
-    data_clean = data.dropna()  # Remove data based on nan's in marked column
+    data_clean = data.dropna()  # Remove data based on nan's in input column
     end_length = len(data_clean)
     
     print(data_clean)
@@ -291,11 +206,7 @@ def main():
     #     # print(data['keep2'].iloc[diff])
     #     print()
     
-    # Save marked data to JSON file      
-    
-    # data_clean.to_json("temp_file.json", orient='records', lines=True)
-    # temp_file = data_clean.to_json(orient='records', lines=True)
-    
+    # Save marked data to JSON file         
     import json
     # l = []
     data_entries = data_clean.to_dict(orient = 'records')
@@ -304,18 +215,12 @@ def main():
             d = {}
             d["definition"] = line
             f.write(json.dumps(d, sort_keys=False) + '\n') 
-    #         l.append(d)
-    # json.dumps(l)
-    
-    # Add additional dict layer specifying task
-    # with open("temp_file.json", 'r') as f:
-    #     file_lines = [''.join(['{"definition":', line.strip(), "}", '\n']) for line in f.readlines()]
-    
-    # with open(args.output_file, 'w') as f:
-    #     f.writelines(file_lines) 
     
     print("Formatted data saved in", args.output_file)
 
 if __name__ == "__main__":
-
-    main()
+    
+    # Parse the arguments
+    args = parse_args()
+    
+    main(args)
